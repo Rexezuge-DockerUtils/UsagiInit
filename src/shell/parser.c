9cdcb27ffd4939e9ae07a59b7db0eb2c707b0156
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-04-24 20:44:00 +0000

    FEAT: parser handles double quotes

--- src/shell/parser.c
+++ src/shell/parser.c
@@ -1,12 +1,43 @@
 #include "shell/parser.h"
+#include <stdbool.h>
 #include <string.h>
 
 void parse_command(char *line, char **args) {
   int i = 0;
-  char *token = strtok(line, " \t\n");
-  while (token != NULL && i < MAX_ARGS - 1) {
-    args[i++] = token;
-    token = strtok(NULL, " \t\n");
+  bool in_quote = false;
+  char *start = line;
+  char *current = line;
+
+  while (*current && i < MAX_ARGS - 1) {
+    if (*current == '"') {
+      in_quote = !in_quote;
+      if (!in_quote) {
+        // End of quoted section
+        *current = '\0';
+        args[i++] = start;
+        start = current + 1;
+      } else {
+        // Start of quoted section
+        start = current + 1;
+      }
+      current++;
+    } else if (!in_quote &&
+               (*current == ' ' || *current == '\t' || *current == '\n')) {
+      if (current != start) {
+        *current = '\0';
+        args[i++] = start;
+      }
+      current++;
+      start = current;
+    } else {
+      current++;
+    }
   }
+
+  // Add the last argument if there's any
+  if (current != start && i < MAX_ARGS - 1) {
+    args[i++] = start;
+  }
+
   args[i] = NULL;
 }
