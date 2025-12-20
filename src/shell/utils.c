056997908d8e22991d10c0c627745b0586af838a
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-12-20 16:00:46 +0000

    FEAT: stderr redirection support

--- src/shell/utils.c
+++ src/shell/utils.c
@@ -26,6 +26,15 @@ void handle_redirection(char **args) {
       dup2(fd, STDOUT_FILENO);
       close(fd);
       args[i] = NULL;
+    } else if (strcmp(args[i], "2>") == 0) {
+      int fd = open(args[i + 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+      if (fd < 0) {
+        perror("open for stderr");
+        exit(EXIT_FAILURE);
+      }
+      dup2(fd, STDERR_FILENO);
+      close(fd);
+      args[i] = NULL;
     }
   }
 }
